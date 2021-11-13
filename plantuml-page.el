b07d8d2e0bc3b7c88cf4ba89583a99afc60465d1
Zeyu Chen <xinyifly@live.cn>
2021-11-14 02:57:16 +0800

    Initial commit

---
+++ plantuml-page.el
@@ -0,0 +1,23 @@
+(defun plantuml-page-preview (prefix)
+  (interactive "p")
+  (let ((p (point)))
+    (plantuml-page-select)
+    (plantuml-preview prefix)
+    (deactivate-mark)
+    (goto-char p)))
+(defun plantuml-page-next ()
+  (interactive) (re-search-forward "\\(newpage\\|@enduml\\)" nil 1))
+(defun plantuml-page-previous ()
+  (interactive) (re-search-backward "\\(newpage\\|@startuml\\)" nil 1))
+(defun plantuml-page-select ()
+  (when (plantuml-page-previous) (goto-char (match-end 0)))
+  (set-mark-command nil)
+  (when (plantuml-page-next) (goto-char (match-beginning 0))))
+
+(eval-after-load 'plantuml-mode
+  (lambda ()
+    (define-key plantuml-mode-map (kbd "C-c C-c") 'plantuml-page-preview)
+    (define-key plantuml-mode-map (kbd "C-c C-n") 'plantuml-page-next)
+    (define-key plantuml-mode-map (kbd "C-c C-p") 'plantuml-page-previous)))
+
+(provide 'plantuml-page)
